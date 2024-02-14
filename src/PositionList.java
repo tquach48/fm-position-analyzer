09f373a18a1ff1d3358ea31d8ff15e165bb2735e
terry <qh384786@gmail.com>
2024-02-14 17:39:50 -0400

    upload initial methods

---
+++ src/PositionList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class PositionList {
+    private ArrayList<PositionX> position_list;
+    public PositionList(){
+       position_list = new ArrayList<>();
+    }
+    public ArrayList<PositionX> getPosition_list(){
+        return position_list;
+    }
+    public void add_all_positions(){}
+    public void add_LB(){}
+    public void add_LCB(){}
+    public void add_CCB(){}
+    public void add_RCB(){}
+    public void add_RB(){}
+    public void add_LWB(){}
+    public void add_LDM(){}
+    public void add_CDM(){}
+    public void add_RDM(){}
+    public void add_RWB(){}
+    public void add_LM(){}
+    public void add_LCM(){}
+    public void add_CCM(){}
+    public void add_RCM(){}
+    public void add_RM(){}
+    public void add_LW(){}
+    public void add_LAM(){}
+    public void add_CAM(){}
+    public void add_RAM(){}
+    public void add_RW(){}
+    public void add_LST(){}
+    public void add_CST(){}
+    public void add_RST(){}
+
+}
